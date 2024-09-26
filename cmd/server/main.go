c142c59ee1809fff9a121abe30aa00541e89be31
darleet <darleet@mail.ru>
2024-09-27 01:32:35 +0300

    main ref: add logger to main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,9 +5,19 @@ import (
 	"os"
 
 	"github.com/go-park-mail-ru/2024_2_deadlock/internal/cmd"
+	"github.com/go-park-mail-ru/2024_2_deadlock/internal/depgraph"
 )
 
 func main() {
+	dg := depgraph.NewDepGraph()
+	logger, err := dg.GetLogger()
+	if err != nil {
+		fmt.Println("get logger: %w", err)
+		os.Exit(1)
+	}
+
+	logger.Info("Starting server...")
+
 	rootCmd, err := cmd.InitRunCommand()
 	if err != nil {
 		fmt.Println("init run command: %w", err)
