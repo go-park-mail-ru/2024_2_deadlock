5ba829a84615222a1c4e1086beb45371e59f054a
darleet <darleet@mail.ru>
2024-09-27 01:24:21 +0300

    cmd feat: implement cobra run command

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/go-park-mail-ru/2024_2_deadlock/internal/cmd"
+	"os"
+)
+
 func main() {
-	print("hello world")
+	rootCmd, err := cmd.InitRunCommand()
+	if err != nil {
+		fmt.Println("init run command: %w", err)
+		os.Exit(1)
+	}
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println("execute: %w", err)
+		os.Exit(1)
+	}
 }
---
+++ internal/cmd/run.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/go-park-mail-ru/2024_2_deadlock/internal/depgraph"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type RunArgs struct {
+	EnvPath string
+}
+
+func InitRunCommand() (*cobra.Command, error) {
+	args := RunArgs{}
+	cmd := &cobra.Command{
+		Use:   "run",
+		Short: "Starts server",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			dg := depgraph.NewDepGraph()
+			logger, _ := dg.GetLogger()
+
+			viper.SetConfigFile(args.EnvPath)
+
+			var config Config
+
+			err := viper.ReadInConfig()
+			if err != nil {
+				logger.Warn("Config file Not Found. Using cli arguments")
+			} else {
+				logger.Debug("Using config file")
+				err = viper.Unmarshal(&config)
+				if err != nil {
+					return err
+				}
+			}
+
+			logger.Debugw(
+				"Got config",
+				"args", args,
+			)
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&args.EnvPath, "config", "c", ".env", ".env file path")
+
+	return cmd, nil
+}
