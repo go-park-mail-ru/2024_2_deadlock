79a84207570afce0aefd2ccedc592236a4b70f8c
darleet <darleet@mail.ru>
2024-09-29 22:15:30 +0300

    utils ref: remove redundant funcs

--- pkg/utils/json.go
+++ pkg/utils/json.go
@@ -3,32 +3,11 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/go-park-mail-ru/2024_2_deadlock/pkg/errors"
 )
 
-type ResponseBody struct {
-	Body interface{} `json:"body"`
-}
-
-func SendError(w http.ResponseWriter, err errors.RestErr) {
-	w.WriteHeader(err.Status())
+func EncodeBody(w http.ResponseWriter, v interface{}) error {
 	encoder := json.NewEncoder(w)
-	_ = encoder.Encode(err)
-}
-
-func SendBody(w http.ResponseWriter, v interface{}) error {
-	encoder := json.NewEncoder(w)
-
-	body := new(ResponseBody)
-	body.Body = v
-
-	if err := encoder.Encode(body); err != nil {
-		SendError(w, errors.NewInternalServerError(err))
-		return err
-	}
-
-	return nil
+	return encoder.Encode(v)
 }
 
 func DecodeBody(r *http.Request, v interface{}) error {
