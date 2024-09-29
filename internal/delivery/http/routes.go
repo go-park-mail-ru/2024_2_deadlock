6390c0545a40e6c27b37d9654852209384760656
darleet <darleet@mail.ru>
2024-09-29 20:23:11 +0300

    auth feat: implement route stubs

--- internal/delivery/http/routes.go
+++ internal/delivery/http/routes.go
@@ -1,5 +1,13 @@
 package http
 
+import "github.com/gorilla/mux"
+
 func (s *Server) makeRoutes() {
-	// TODO: add routes
+	s.mux = mux.NewRouter().StrictSlash(true)
+
+	v1 := s.mux.PathPrefix("/api/v1").Subrouter()
+
+	v1.HandleFunc("/login", s.Login)
+	v1.HandleFunc("/logout", s.Logout)
+	v1.HandleFunc("/register", s.Register)
 }
