365162ba3b560e6ef3e7e8fac5ccdaf942f105c8
darleet <darleet@mail.ru>
2024-09-29 13:39:32 +0300

    server ref: ref cobra setup

--- cmd/server/app/run.go
+++ cmd/server/app/run.go
@@ -3,26 +3,16 @@ package app
 import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 
-	"github.com/go-park-mail-ru/2024_2_deadlock/internal/bootstrap"
 	"github.com/go-park-mail-ru/2024_2_deadlock/internal/depgraph"
-)
-
-const (
-	cmdConfigName      = "config"
-	cmdConfigShorthand = "c"
-	cmdConfigValue     = "dev.yaml"
-	cmdConfigUsage     = ".yaml file path"
 )
 
 type RunArgs struct {
 	EnvPath string
 }
 
-func InitRunCommand() (*cobra.Command, error) {
-	args := RunArgs{}
-	cmd := &cobra.Command{
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
 		Use:   "run",
 		Short: "Starts server",
 		RunE: func(cmd *cobra.Command, _ []string) error {
@@ -32,31 +22,9 @@ func InitRunCommand() (*cobra.Command, error) {
 				return errors.Wrap(err, "get logger")
 			}
 
-			viper.SetConfigFile(args.EnvPath)
-
-			var cfg bootstrap.Config
-
-			err = viper.ReadInConfig()
-			if err != nil {
-				logger.Warn("Config file Not Found. Using cli arguments")
-			} else {
-				logger.Debug("Using config file")
-				err = viper.Unmarshal(&cfg)
-				if err != nil {
-					return errors.Wrap(err, "unmarshal config")
-				}
-			}
-
-			logger.Debugw(
-				"Got config",
-				"args", args,
-			)
+			logger.Info("Starting server...")
 
 			return nil
 		},
-	}
-
-	cmd.Flags().StringVarP(&args.EnvPath, cmdConfigName, cmdConfigShorthand, cmdConfigValue, cmdConfigUsage)
-
-	return cmd, nil
+	})
 }
