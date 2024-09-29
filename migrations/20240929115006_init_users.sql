d141c0fffd03899cd6c025a768ed47e4bbe7a179
darleet <darleet@mail.ru>
2024-09-29 14:56:11 +0300

    migrations feat: add auth schema init

---
+++ migrations/20240929115006_init_users.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE SCHEMA IF NOT EXISTS auth;
+
+CREATE TABLE IF NOT EXISTS auth.users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    email TEXT
+        UNIQUE
+        CONSTRAINT email_length CHECK ( CHAR_LENGTH(email) <= 255 )
+        NOT NULL,
+    password TEXT
+        CONSTRAINT password_length CHECK ( CHAR_LENGTH(password) <= 255 )
+        NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS auth.users;
+DROP SCHEMA IF EXISTS auth;
+-- +goose StatementEnd
