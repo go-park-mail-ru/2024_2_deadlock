33604aec53581ef4165bdd0fe8e13b22f7e325bb
darleet <darleet@mail.ru>
2024-09-29 20:23:11 +0300

    auth feat: implement route stubs

---
+++ internal/delivery/http/auth.go
@@ -0,0 +1,15 @@
+package http
+
+import "net/http"
+
+func (s *Server) Login(w http.ResponseWriter, r *http.Request) {
+	panic("Not implemented!")
+}
+
+func (s *Server) Logout(w http.ResponseWriter, r *http.Request) {
+	panic("Not implemented!")
+}
+
+func (s *Server) Register(w http.ResponseWriter, r *http.Request) {
+	panic("Not implemented!")
+}
