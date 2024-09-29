00004d1bd36a7e088dbb088af6be80d714947b17
darleet <darleet@mail.ru>
2024-09-29 23:07:16 +0300

    server feat: implement auth handlers

--- internal/delivery/http/auth.go
+++ internal/delivery/http/auth.go
@@ -1,15 +1,117 @@
 package http
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+
+	"github.com/go-park-mail-ru/2024_2_deadlock/internal/domain"
+	"github.com/go-park-mail-ru/2024_2_deadlock/pkg/resterr"
+	"github.com/go-park-mail-ru/2024_2_deadlock/pkg/utils"
+)
+
+type AuthUC interface {
+	Login(ctx context.Context, user domain.UserInput) (domain.SessionID, error)
+	Logout(ctx context.Context, sessionID domain.SessionID) error
+	Register(ctx context.Context, user domain.UserInput) (domain.SessionID, error)
+}
 
 func (s *Server) Login(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented!")
+	defer r.Body.Close()
+
+	input := new(domain.UserInput)
+
+	err := utils.DecodeBody(r, input)
+	if err != nil {
+		s.ProcessBadRequestError(w, err)
+		return
+	}
+
+	sessionID, err := s.uc.Auth.Login(r.Context(), *input)
+	if err != nil && errors.Is(err, resterr.ErrNotFound) {
+		s.log.Errorw("could not login", zap.Error(err))
+		s.SendError(w, resterr.NewNotFoundError("user not found"))
+
+		return
+	} else if err != nil {
+		s.ProcessInternalServerError(w, err)
+		return
+	}
+
+	cookie := &http.Cookie{
+		Name:     "session_id",
+		Value:    string(sessionID),
+		Path:     "/",
+		Expires:  time.Now().Add(12 * time.Hour),
+		HttpOnly: true,
+		Secure:   false, // TODO: add https
+	}
+
+	http.SetCookie(w, cookie)
 }
 
 func (s *Server) Logout(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented!")
+	defer r.Body.Close()
+
+	cookie, err := r.Cookie("session_id")
+	if err != nil {
+		s.log.Errorw("could not get session id from cookies", zap.Error(err))
+		s.SendError(w, resterr.NewUnauthorizedError(err))
+
+		return
+	}
+
+	err = s.uc.Auth.Logout(r.Context(), domain.SessionID(cookie.Value))
+	if err != nil {
+		s.ProcessInternalServerError(w, err)
+		return
+	}
+
+	cookie = &http.Cookie{
+		Name:     "session_id",
+		Value:    "",
+		Path:     "/",
+		Expires:  time.Now().Add(-1 * time.Hour),
+		HttpOnly: true,
+		Secure:   false, // TODO: add https
+	}
+
+	http.SetCookie(w, cookie)
 }
 
 func (s *Server) Register(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented!")
+	defer r.Body.Close()
+
+	input := new(domain.UserInput)
+
+	err := utils.DecodeBody(r, input)
+	if err != nil {
+		s.ProcessBadRequestError(w, err)
+		return
+	}
+
+	sessionID, err := s.uc.Auth.Register(r.Context(), *input)
+	if err != nil && errors.Is(err, resterr.ErrConflict) {
+		s.log.Errorw("could not register", zap.Error(err))
+		s.SendError(w, resterr.NewConflictError("user already exists"))
+
+		return
+	} else if err != nil {
+		s.ProcessInternalServerError(w, err)
+		return
+	}
+
+	cookie := &http.Cookie{
+		Name:     "session_id",
+		Value:    string(sessionID),
+		Path:     "/",
+		Expires:  time.Now().Add(12 * time.Hour),
+		HttpOnly: true,
+		Secure:   false, // TODO: add https
+	}
+
+	http.SetCookie(w, cookie)
 }
