329b58d43715b8290139ee47a50628fdd6f89fa8
darleet <darleet@mail.ru>
2024-09-30 18:02:39 +0300

    auth feat: add repo interfaces

--- internal/usecase/auth/auth.go
+++ internal/usecase/auth/auth.go
@@ -7,8 +7,25 @@ import (
 	"github.com/go-park-mail-ru/2024_2_deadlock/internal/usecase"
 )
 
+type SessionRepository interface {
+	CreateSession(ctx context.Context, userID domain.UserID) (domain.SessionID, error)
+	DeleteSession(ctx context.Context, sessionID domain.SessionID) error
+	GetUserID(ctx context.Context, sessionID domain.SessionID) (domain.UserID, error)
+}
+
+type UserRepository interface {
+	GetByID(ctx context.Context, userID domain.UserID) (domain.User, error)
+	Create(ctx context.Context, user domain.UserInput) (domain.UserID, error)
+}
+
+type Repositories struct {
+	Session SessionRepository
+	User    UserRepository
+}
+
 type Usecase struct {
 	usecase.CommonUC
+	repo Repositories
 }
 
 func (uc *Usecase) Login(ctx context.Context, user domain.UserInput) (domain.SessionID, error) {
