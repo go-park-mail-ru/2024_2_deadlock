7ea59bd0b0b707fdac512c058ba6591d70394ab9
darleet <darleet@mail.ru>
2024-09-30 05:49:15 +0300

    auth feat: add uc stubs

---
+++ internal/usecase/auth/auth.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/go-park-mail-ru/2024_2_deadlock/internal/domain"
+	"github.com/go-park-mail-ru/2024_2_deadlock/internal/usecase"
+)
+
+type Usecase struct {
+	usecase.CommonUC
+}
+
+func (uc *Usecase) Login(ctx context.Context, user domain.UserInput) (domain.SessionID, error) {
+	panic("implement me")
+}
+
+func (uc *Usecase) Logout(ctx context.Context, sessionID domain.SessionID) error {
+	panic("implement me")
+}
+
+func (uc *Usecase) Register(ctx context.Context, user domain.UserInput) (domain.SessionID, error) {
+	panic("implement me")
+}
+
+func (uc *Usecase) CurrentUser(ctx context.Context, sessionID domain.SessionID) (domain.User, error) {
+	panic("implement me")
+}
