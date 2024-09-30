d8f881cf31d582e83fe1d60f68484109cd1cfb35
darleet <darleet@mail.ru>
2024-09-30 18:28:01 +0300

    domain ref: change user id type

--- internal/domain/auth.go
+++ internal/domain/auth.go
@@ -1,7 +1,5 @@
 package domain
 
-type UserID int
-
 type UserInput struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,regexp=/^[a-zA-Z0-9?!_\\-*$]{6,255}$/"`
