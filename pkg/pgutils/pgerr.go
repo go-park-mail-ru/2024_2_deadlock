9210138a2f8c116a9d4e9d3c1dbd1b03765134ca
darleet <darleet@mail.ru>
2024-10-01 20:13:38 +0300

    pgutils ref: rename pgerr and implement CancelTxOnErr

--- pkg/pgutils/pgerr.go
+++ pkg/pgutils/pgerr.go
@@ -1,6 +1,9 @@
-package pgerr
+package pgutils
 
 import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/pkg/errors"
 )
@@ -8,4 +11,12 @@ import (
 func IsAlreadyExistsError(err error) bool {
 	var pgErr *pgconn.PgError
 	return errors.As(err, &pgErr) && pgErr.Code == "23505"
+}
+
+func CancelTxOnErr(ctx context.Context, tx pgx.Tx, err error) {
+	if err != nil {
+		_ = tx.Rollback(ctx)
+	} else {
+		_ = tx.Commit(ctx)
+	}
 }
