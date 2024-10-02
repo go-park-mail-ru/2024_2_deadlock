3be7e90b8a5805e287ac16fb5bef044322a4e2e7
darleet <darleet@mail.ru>
2024-10-02 17:09:52 +0300

    migrations ref: add not null constaints for article

--- migrations/20241002094918_init_article.sql
+++ migrations/20241002094918_init_article.sql
@@ -4,9 +4,9 @@ CREATE SCHEMA IF NOT EXISTS feed;
 
 CREATE TABLE IF NOT EXISTS feed.article (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title TEXT,
+    title TEXT NOT NULL,
     media_url TEXT,
-    body TEXT
+    body TEXT NOT NULL
 );
 -- +goose StatementEnd
 
